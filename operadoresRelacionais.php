9fc391cbeaad2adf98428a9d9b0740d41eb63806
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2021-11-12 19:15:35 -0300

    correction

--- operadoresRelacionais.php
+++ operadoresRelacionais.php
@@ -9,14 +9,14 @@
 <body>
     <?php 
     echo "<h1>Exercícios sobre operadores relacionais</h1>";
-    echo "<h2>Exercício 1 - Operador Unário</h2>";
+    echo "<h2>Exercício 1 - Operador Ternário</h2>";
     $nota1 = $_GET["n1"];
     $nota2 = $_GET["n2"];
     $media = ($nota1+$nota2)/2;
     echo "A média das notas $nota1 e $nota2 é de $media";
     echo "<br>A situação do aluno é ". ($media>=6? "APROVADO":"REPROVADO");
 
-    echo "<h2>Exercício 2 - Operador unário</h2>";
+    echo "<h2>Exercício 2 - Operador Ternário</h2>";
     $idade = $_GET["i"];
     echo "A pessoa tem $idade anos e tem ". ($idade <= 65 && $idade>=18? "VOTO OBRIGATÓRIO": "VOTO NÃO OBRIGATÓRIO");
     ?>
